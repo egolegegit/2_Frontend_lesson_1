4757083a5a402490c6f3c99a9038ae1c323d48b5
Рыбников Андрей Анатольевич <43681241+egolegegit4226@users.noreply.github.com>
2021-08-16 14:53:36 +0700

    init commit

---
+++ first-react-app/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+const element = <h1>hello world меня зовут *Рыбников Андрей*</h1>
+
+console.log(element)
+ReactDom.render(element, document.getElementById('root'))
